54aaeeaa739a100ece746e2925070e814d834d6c
Andrés Gallo <andresgallod.i@gmail.com>
2020-08-08 20:02:01 -0400

    update

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -7,8 +7,7 @@
       sigils = document.querySelectorAll(".sigilContainer"),
       houseImages = document.querySelector("#houseImages"),
       houseName = document.querySelector(".house-name"),
-      houseBio = document.querySelector(".house-info"),
-      prover = 0;
+      houseBio = document.querySelector(".house-info");
 
   const houseInfo = [
     ["Stark", `House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`],
